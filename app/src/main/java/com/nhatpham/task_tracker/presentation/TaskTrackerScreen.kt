b5cf9ced498a72c341a8ce898193611737742379
Lyon <fopop2@gmail.com>
2025-06-22 08:38:54 +0700

    basic ui finished

--- app/src/main/java/com/nhatpham/task_tracker/presentation/TaskTrackerScreen.kt
+++ app/src/main/java/com/nhatpham/task_tracker/presentation/TaskTrackerScreen.kt
@@ -1,26 +1,37 @@
 package com.nhatpham.task_tracker.presentation
 
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Clear
 import androidx.compose.material.icons.filled.KeyboardArrowDown
 import androidx.compose.material.icons.filled.KeyboardArrowUp
 import androidx.compose.material3.Button
+import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.RadioButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.window.Dialog
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.nhatpham.task_tracker.domain.model.PriorityLevel
 import com.nhatpham.task_tracker.domain.model.Task
@@ -35,6 +46,8 @@ fun TaskTrackerScreen(
 ) {
     val state by viewModel.state.collectAsState()
 
+    var showDialog by remember { mutableStateOf(false) }
+
     var inputUiState by remember { mutableStateOf(TaskInputUiState()) }
 
     val taskActions = TaskActions(onDelete = { viewModel.onEvent(DeleteTask(it)) },
@@ -46,116 +59,144 @@ fun TaskTrackerScreen(
         viewModel.onEvent(TaskTrackerEvent.LoadAllTasks)
     }
 
-    when (state.allTasksResource) {
-        is Resource.Success -> {
-            val tasks = (state.allTasksResource as Resource.Success<List<Task>>).data
-            val inProgressTasks = tasks.filter { !it.isCompleted }
-            val completedTasks = tasks.filter { it.isCompleted }
-            Column(
-                modifier = modifier
-            ) {
-                InProgressSection(
-                    tasks = inProgressTasks, taskActions = taskActions
-                )
-                CompletedSection(
-                    tasks = completedTasks, taskActions = taskActions
-                )
-                TaskInputBox(
-                    inputState = inputUiState,
-                    onTitleChange = { inputUiState = inputUiState.copy(newTaskTitle = it) },
-                    onContentChange = { inputUiState = inputUiState.copy(newTaskContent = it) },
-                    onAddClick = {
-                        viewModel.onEvent(
-                            TaskTrackerEvent.CreateTask(
-                                Task(
-                                    taskId = "",
-                                    title = inputUiState.newTaskTitle,
-                                    content = inputUiState.newTaskContent,
-                                    priorityLevel = inputUiState.selectedPriority
-                                )
-                            )
-                        )
-                    },
-                    isAddingTask = state.createTaskResource is Resource.Loading,
-                    errorMessage = (state.createTaskResource as? Resource.Error)?.message,
-                    modifier = modifier
-                )
-            }
-        }
-
-        is Resource.Error -> {
-
-        }
-
-        Resource.Idle -> {
-
-        }
-
-        is Resource.Loading -> {
-
+    LaunchedEffect(state.createTaskResource) {
+        if (state.createTaskResource is Resource.Success) {
+            showDialog = false
         }
     }
 
+    Scaffold(floatingActionButton = {
+        FloatingActionButton(onClick = { showDialog = true }) {
+            Icon(Icons.Default.Add, contentDescription = "Add")
+        }
+    }) { innerPadding ->
+        when (state.allTasksResource) {
+            is Resource.Success -> {
+                val tasks = (state.allTasksResource as Resource.Success<List<Task>>).data
+                val inProgressTasks = tasks.filter { !it.isCompleted }
+                val completedTasks = tasks.filter { it.isCompleted }
+                Column(
+                    modifier = modifier.padding(innerPadding)
+                ) {
+                    InProgressSection(
+                        tasks = inProgressTasks, taskActions = taskActions
+                    )
+                    CompletedSection(
+                        tasks = completedTasks, taskActions = taskActions
+                    )
+                    if (showDialog) {
+                        TaskInputDialogue(
+                            inputState = inputUiState,
+                            onTitleChange = { inputUiState = inputUiState.copy(newTaskTitle = it) },
+                            onContentChange = {
+                                inputUiState = inputUiState.copy(newTaskContent = it)
+                            },
+                            onAddClick = {
+                                viewModel.onEvent(
+                                    TaskTrackerEvent.CreateTask(
+                                        Task(
+                                            taskId = "",
+                                            title = inputUiState.newTaskTitle,
+                                            content = inputUiState.newTaskContent,
+                                            priorityLevel = inputUiState.selectedPriority
+                                        )
+                                    )
+                                )
+                            },
+                            onDismiss = {
+                                showDialog = false
+                            },
+                            isAddingTask = state.createTaskResource is Resource.Loading,
+                            errorMessage = (state.createTaskResource as? Resource.Error)?.message,
+                            modifier = modifier
+                        )
+                    }
+                }
+            }
 
+            is Resource.Error -> {
+
+            }
+
+            Resource.Idle -> {
+
+            }
+
+            is Resource.Loading -> {
+
+            }
+        }
+    }
 }
 
 @Composable
 private fun InProgressSection(
-    tasks: List<Task>, taskActions: TaskActions
+    tasks: List<Task>, taskActions: TaskActions, modifier: Modifier = Modifier
 ) {
-    Text(text = "Task Tracker")
-    tasks.forEach { task ->
-        TaskItem(
-            task = task,
-            onDeleteClick = taskActions.onDelete,
-            onToggleComplete = taskActions.onToggleComplete
-        )
+    Column(modifier = modifier.fillMaxWidth()) {
+        Text(text = "In Progress")
+        tasks.forEach { task ->
+            TaskItem(
+                task = task,
+                onDeleteClick = taskActions.onDelete,
+                onToggleComplete = taskActions.onToggleComplete
+            )
+        }
     }
 }
 
 @Composable
 private fun CompletedSection(
-    tasks: List<Task>, taskActions: TaskActions
+    tasks: List<Task>, taskActions: TaskActions, modifier: Modifier = Modifier
 ) {
-    Text(text = "Task Tracker")
-    tasks.forEach { task ->
-        TaskItem(
-            task = task,
-            onDeleteClick = taskActions.onDelete,
-            onToggleComplete = taskActions.onToggleComplete
-        )
+    Column(modifier = modifier.fillMaxWidth()) {
+        Text(text = "Completed")
+        tasks.forEach { task ->
+            TaskItem(
+                task = task,
+                onDeleteClick = taskActions.onDelete,
+                onToggleComplete = taskActions.onToggleComplete
+            )
+        }
     }
 }
 
 @Composable
-private fun TaskInputBox(
+private fun TaskInputDialogue(
     inputState: TaskInputUiState,
     onTitleChange: (String) -> Unit,
     onContentChange: (String) -> Unit,
     onAddClick: () -> Unit,
+    onDismiss: () -> Unit,
     isAddingTask: Boolean,
     errorMessage: String?,
     modifier: Modifier = Modifier
 ) {
-    Column(modifier = modifier) {
-        Text(text = "Task Tracker")
-        OutlinedTextField(value = inputState.newTaskTitle,
-            onValueChange = onTitleChange,
-            label = { Text(text = "Task Title") })
-        OutlinedTextField(value = inputState.newTaskContent,
-            onValueChange = onContentChange,
-            label = { Text(text = "Task Content") })
-        if (errorMessage != null) {
-            Text(
-                text = "Error occurred: $errorMessage", color = MaterialTheme.colorScheme.error
-            )
-        }
-        Button(
-            onClick = onAddClick,
-            enabled = !isAddingTask,
+    Dialog(
+        onDismissRequest = onDismiss
+    ) {
+        Surface {
+            Column(modifier = modifier) {
+                OutlinedTextField(value = inputState.newTaskTitle,
+                    onValueChange = onTitleChange,
+                    label = { Text(text = "Task Title") })
+                OutlinedTextField(value = inputState.newTaskContent,
+                    onValueChange = onContentChange,
+                    label = { Text(text = "Task Content") })
+                if (errorMessage != null) {
+                    Text(
+                        text = "Error occurred: $errorMessage",
+                        color = MaterialTheme.colorScheme.error
+                    )
+                }
+                Button(
+                    onClick = onAddClick,
+                    enabled = !isAddingTask,
 
-            ) {
-            Text(text = "Add Task")
+                    ) {
+                    Text(text = "Add Task")
+                }
+            }
         }
     }
 }
@@ -168,28 +209,51 @@ fun TaskItem(
     modifier: Modifier = Modifier
 ) {
     val isExpanded = remember { mutableStateOf(false) }
-    Row(modifier = modifier) {
-        Text(text = task.title)
-        IconButton(onClick = { isExpanded.value = !isExpanded.value }) {
-            Icon(
-                imageVector = if (isExpanded.value) Icons.Default.KeyboardArrowDown else Icons.Default.KeyboardArrowUp,
-                contentDescription = if (isExpanded.value) {
-                    "Show less"
-                } else {
-                    "Show more"
+    Column(modifier = modifier) {
+        Row(
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text(text = task.title)
+            Row(modifier = Modifier) {
+                IconButton(onClick = { isExpanded.value = !isExpanded.value }) {
+                    Icon(
+                        imageVector = if (isExpanded.value) Icons.Default.KeyboardArrowDown else Icons.Default.KeyboardArrowUp,
+                        contentDescription = if (isExpanded.value) {
+                            "Show less"
+                        } else {
+                            "Show more"
+                        }
+                    )
                 }
-            )
+                RadioButton(selected = task.isCompleted, onClick = { onToggleComplete(task) })
+                IconButton(onClick = { onDeleteClick(task.taskId) }) {
+                    Icon(
+                        imageVector = Icons.Default.Clear, contentDescription = "Clear"
+                    )
+                }
+            }
         }
-        RadioButton(selected = task.isCompleted, onClick = { onToggleComplete(task) })
-        IconButton(onClick = { onDeleteClick(task.taskId) }) {
-            Icon(
-                imageVector = Icons.Default.Clear, contentDescription = "Clear"
-            )
-        }
+        CollapsibleSection(
+            isExpanded = isExpanded.value,
+            taskContent = task.content,
+        )
     }
 }
 
-
+@Composable
+fun CollapsibleSection(
+    isExpanded: Boolean,
+    modifier: Modifier = Modifier,
+    taskContent: String?,
+) {
+    AnimatedVisibility(isExpanded) {
+        Row(modifier = modifier) {
+            Text(text = taskContent ?: "Empty Content")
+        }
+    }
+}
 
 private data class TaskInputUiState(
     val newTaskTitle: String = "",
