4ac00a2c59273791fd8ac5294ed94a99c0f54065
Lyon <fopop2@gmail.com>
2025-06-22 08:45:01 +0700

    added lazycolumn and animatedvisibility

--- app/src/main/java/com/nhatpham/task_tracker/presentation/TaskTrackerScreen.kt
+++ app/src/main/java/com/nhatpham/task_tracker/presentation/TaskTrackerScreen.kt
@@ -1,11 +1,13 @@
 package com.nhatpham.task_tracker.presentation
 
 import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Clear
@@ -25,7 +27,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
@@ -133,14 +134,23 @@ fun TaskTrackerScreen(
 private fun InProgressSection(
     tasks: List<Task>, taskActions: TaskActions, modifier: Modifier = Modifier
 ) {
-    Column(modifier = modifier.fillMaxWidth()) {
+    var showTasks by remember { mutableStateOf(true) }
+    Column(
+        modifier = modifier
+            .fillMaxWidth()
+            .clickable(onClick = { showTasks = !showTasks })
+    ) {
         Text(text = "In Progress")
-        tasks.forEach { task ->
-            TaskItem(
-                task = task,
-                onDeleteClick = taskActions.onDelete,
-                onToggleComplete = taskActions.onToggleComplete
-            )
+        AnimatedVisibility(showTasks) {
+            LazyColumn(modifier = Modifier.fillMaxWidth()) {
+                items(tasks.size) { index ->
+                    TaskItem(
+                        task = tasks[index],
+                        onDeleteClick = taskActions.onDelete,
+                        onToggleComplete = taskActions.onToggleComplete
+                    )
+                }
+            }
         }
     }
 }
@@ -149,14 +159,23 @@ private fun InProgressSection(
 private fun CompletedSection(
     tasks: List<Task>, taskActions: TaskActions, modifier: Modifier = Modifier
 ) {
-    Column(modifier = modifier.fillMaxWidth()) {
+    var showTasks by remember { mutableStateOf(true) }
+    Column(
+        modifier = modifier
+            .fillMaxWidth()
+            .clickable(onClick = { showTasks = !showTasks })
+    ) {
         Text(text = "Completed")
-        tasks.forEach { task ->
-            TaskItem(
-                task = task,
-                onDeleteClick = taskActions.onDelete,
-                onToggleComplete = taskActions.onToggleComplete
-            )
+        AnimatedVisibility(showTasks) {
+            LazyColumn(modifier = Modifier.fillMaxWidth()) {
+                items(tasks.size) { index ->
+                    TaskItem(
+                        task = tasks[index],
+                        onDeleteClick = taskActions.onDelete,
+                        onToggleComplete = taskActions.onToggleComplete
+                    )
+                }
+            }
         }
     }
 }
